56207ddbc52a47de2e39fe914e6958db239081ab
GideonBrasil <gidxx_z@yahoo.com>
2019-05-01 08:23:15 -0600

    load module and test HTTP function

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Load logger module
+const logger = require("./logger");
+
+// console.log("logger module", logger);
+logger.log("Hello from Gideon");
+
 // first node hello function
 function sayHello(name) {
   console.log("Hello " + name);
