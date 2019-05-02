0b1c19d74f377296e1353e10317f010e33a3acc0
GideonBrasil <gidxx_z@yahoo.com>
2019-05-02 07:26:57 -0600

    work with events arguments

--- app.js
+++ app.js
@@ -3,49 +3,49 @@ const EventEmmiter = require("events");
 const emitter = new EventEmmiter();
 
 // Register a listener
-emitter.on("messageLogged", function() {
-  console.log("Listener called");
+emitter.on("messageLogged", arg => {
+  console.log("Listener called", arg);
 });
 
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
 
-// take a look at the fs module
-const fs = require("fs");
+// // take a look at the fs module
+// const fs = require("fs");
 
-const files = fs.readdirSync("./");
-console.log("files:", files);
+// const files = fs.readdirSync("./");
+// console.log("files:", files);
 
-fs.readdir("./", function(err, res) {
-  if (err) console.log("Error", err);
-  else console.log("Result", res);
-});
+// fs.readdir("./", function(err, res) {
+//   if (err) console.log("Error", err);
+//   else console.log("Result", res);
+// });
 
-// take a look at os module
-const os = require("os");
+// // take a look at os module
+// const os = require("os");
 
-const totalMemory = os.totalmem();
-const freeMemory = os.freemem();
+// const totalMemory = os.totalmem();
+// const freeMemory = os.freemem();
 
-console.log("Total Memory: " + totalMemory);
-console.log(`Free Memory: ${freeMemory}`);
+// console.log("Total Memory: " + totalMemory);
+// console.log(`Free Memory: ${freeMemory}`);
 
-// Path module test
-const path = require("path");
+// // Path module test
+// const path = require("path");
 
-const pathObj = path.parse(__filename);
-console.log("pathObj:", pathObj);
+// const pathObj = path.parse(__filename);
+// console.log("pathObj:", pathObj);
 
-// Load logger module
-const log = require("./logger");
+// // Load logger module
+// const log = require("./logger");
 
-// console.log("logger module", logger);
-log("Hello from Gideon");
+// // console.log("logger module", logger);
+// log("Hello from Gideon");
 
-// first node hello function
-function sayHello(name) {
-  console.log("Hello " + name);
-}
+// // first node hello function
+// function sayHello(name) {
+//   console.log("Hello " + name);
+// }
 
-sayHello("Gideon");
+// sayHello("Gideon");
 
-console.log(module);
+// console.log(module);
