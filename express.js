c2e80529c41a971818a4a91f8ec010744a9d34d5
GideonBrasil <gidxx_z@yahoo.com>
2019-05-07 07:54:45 -0600

    find objects in array using params | status code 404 convention

--- express.js
+++ express.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 const courses = [
-    { id: 1, name: "course1"}
-    { id: 2, name: "course2"}
-    { id: 3, name: "course3"}
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
+];
 
 app.get("/", (req, res) => {
   res.send("This is an express server baby!");
@@ -17,6 +17,15 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:year/:month", (req, res) => {
   res.send(req.params);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const findCourse = courses.find(
+    course => course.id === parseInt(req.params.id)
+  );
+  if (!findCourse)
+    res.status(404).send("The course with the given ID does not exist.");
+  res.send(findCourse);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
