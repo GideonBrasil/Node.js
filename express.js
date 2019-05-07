74f51cac95c5c7450e9a41493577803009b54c32
GideonBrasil <gidxx_z@yahoo.com>
2019-05-07 07:49:00 -0600

    some express routes practice

---
+++ express.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const courses = [
+    { id: 1, name: "course1"}
+    { id: 2, name: "course2"}
+    { id: 3, name: "course3"}
+]
+
+app.get("/", (req, res) => {
+  res.send("This is an express server baby!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.get("/api/courses/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
